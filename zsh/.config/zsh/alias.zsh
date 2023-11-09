711886fb67451c8456c14374af9f0e94d10c7176
morehush <t@morehush.com>
2023-11-09 18:34:56 -0500

    not worth mentioning

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -19,10 +19,10 @@ alias wgup='wg-quick up wg0'
 alias wgd='wg-quick down wg0'
 
 # tailscale
-alias tsvpn='sudo tailscale up --exit-node=100.112.40.23 --exit-node-allow-lan-access=true --accept-routes'
-alias tsup='sudo tailscale up --accept-routes'
-alias tssrv='sudo tailscale up --ssh --advertise-routes=192.168.2.0/24 --advertise-exit-node'
-alias tsd='sudo tailscale down'
+# alias tsvpn='sudo tailscale up --exit-node=100.112.40.23 --exit-node-allow-lan-access=true --accept-routes'
+# alias tsup='sudo tailscale up --accept-routes'
+# alias tssrv='sudo tailscale up --ssh --advertise-routes=192.168.2.0/24 --advertise-exit-node'
+# alias tsd='sudo tailscale down'
 
 # systemctl
 alias sc='sudo systemctl'
