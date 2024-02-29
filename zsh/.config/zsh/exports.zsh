b4b6550da745e5a09c09d7183423ddfc320531ab
morehush <t@morehush.com>
2024-02-29 10:12:27 -0500

    zsh editor change, again.

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export XDG_CURRENT_DESKTOP="Wayland"
-export EDITOR="nvim"
-export VISUAL="nvim"
+export EDITOR="helix"
+export VISUAL="helix"
 export TERMINAL="kitty"
 export TERM="xterm-256color"
 
