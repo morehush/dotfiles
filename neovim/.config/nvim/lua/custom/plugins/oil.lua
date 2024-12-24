ad28b0f347402e67689c677a32de456e1d585ef4
morehush <t@morehush.com>
2024-12-23 20:45:51 -0500

    neovim plugin changes, add oil.nvim and remove some others

--- neovim/.config/nvim/lua/custom/plugins/oil.lua
+++ neovim/.config/nvim/lua/custom/plugins/oil.lua
@@ -7,5 +7,18 @@ return {
     -- Optional dependencies
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          natural_order = true,
+        },
+        win_options = {
+          wrap = true,
+        }
+      )}
+    end,
   },
 }
