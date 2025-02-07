bbde535f2fbd8afbdf22901f31b0bae4833ddbdc
morehush <t@morehush.com>
2025-02-07 10:17:12 -0500

    lazyvim disable neo-tree and install oil.nvim

---
+++ neovim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          natural_order = true,
+        },
+        win_options = {
+          wrap = true,
+        },
+      })
+    end,
+  },
+}
