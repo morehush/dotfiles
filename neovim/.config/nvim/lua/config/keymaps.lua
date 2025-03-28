fd343c15c632e73807349b276725c36ae876764f
morehush <t@morehush.com>
2025-03-28 14:32:37 -0400

    add keymap to neovim config

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
