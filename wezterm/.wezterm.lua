0b7fdbff8be181e6f62d514df6dfd0c5337587d6
morehush <t@morehush.com>
2025-05-14 13:11:53 -0400

    wezterm: config tweaks

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,12 +7,23 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config = {
+	enable_kitty_graphics = true,
+	max_fps = 120,
+	initial_cols = 120,
+	initial_rows = 28,
 
--- or, changing the font size and color scheme.
-config.font_size = 11
-config.color_scheme = "Tokyo Night"
+	-- or, changing the font size and color scheme.
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font_size = 11,
+	color_scheme = "Tokyo Night",
 
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 15,
+		bottom = 0,
+	},
+}
 -- Finally, return the configuration to wezterm:
 return config
