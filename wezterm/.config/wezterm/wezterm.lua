14274fc674a9745e1356b43e82b03b40794ba894
morehush <t@morehush.com>
2025-05-15 12:59:14 -0400

    wezterm:change config location

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,19 +4,19 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
 config = {
 	enable_kitty_graphics = true,
 	max_fps = 120,
 	initial_cols = 120,
 	initial_rows = 28,
 
-	-- or, changing the font size and color scheme.
+	-- Font, font size, and color scheme
 	font = wezterm.font("JetBrainsMono Nerd Font"),
 	font_size = 11,
 	color_scheme = "Tokyo Night",
+
+	-- Appearance
+	hide_tab_bar_if_only_one_tab = true,
 
 	window_padding = {
 		left = 2,
