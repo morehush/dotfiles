bfc5a806d5acbd2044b07003c0ee9098ed5715ee
morehush <t@morehush.com>
2025-05-15 19:06:28 -0400

    wezterm:removed padding section in config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -33,13 +33,6 @@ config = {
 	-- hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.8,
 	use_fancy_tab_bar = false,
-
-	window_padding = {
-		left = 2,
-		right = 2,
-		top = 0,
-		bottom = 0,
-	},
 }
 
 -- Key bindings configuration
