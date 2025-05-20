109cc4b710c6f799c3cb40a466feb1bbd9716ce3
morehush <t@morehush.com>
2025-05-20 08:32:34 -0400

    wezterm:commented out background opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -26,8 +26,14 @@ config = {
 
 	-- Appearance
 	-- hide_tab_bar_if_only_one_tab = true,
-	window_background_opacity = 0.8,
+	-- window_background_opacity = 0.8,
 	use_fancy_tab_bar = false,
+	-- window_padding = {
+	-- 	left = 0,
+	-- 	right = 0,
+	-- 	top = 0,
+	-- 	bottom = 0,
+	-- },
 }
 
 -- Key bindings configuration
