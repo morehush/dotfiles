7f2e8265a8b5b5e9de0032aa2c680e201a6426bb
morehush <t@morehush.com>
2025-10-07 10:48:49 -0400

    zsh:remove un-needed aliases

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -102,14 +102,8 @@ alias genkey='ssh-keygen -t rsa -b 4096'
 
 # yt-dlp
 alias ytd='yt-dlp'
-alias yta-aac='yt-dlp --extract-audio --audio-format aac '
-alias yta-best='yt-dlp --extract-audio --audio-format best '
-alias yta-flac='yt-dlp --extract-audio --audio-format flac '
-alias yta-m4a='yt-dlp --extract-audio --audio-format m4a '
-alias yta-mp3='yt-dlp --extract-audio --audio-format mp3 '
-alias yta-opus='yt-dlp --extract-audio --audio-format opus '
-alias yta-vorbis='yt-dlp --extract-audio --audio-format vorbis '
-alias yta-wav='yt-dlp --extract-audio --audio-format wav '
+alias ytf='yt-dlp --ignore-config --downloader aria2c -N 4 -a files'
+alias ytaria='yt-dlp --downloader aria2c -N 4 -a files'
 alias ytv-best='yt-dlp -f bestvideo+bestaudio '
 
 # docker compose
