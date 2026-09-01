ea2942e644863434f4ae784de415d6b00d3a30af
morehush <t@morehush.com>
2026-09-01 10:53:04 -0400

    fish:removed some old functions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,37 +36,6 @@ function fish_user_key_bindings
     fish_vi_key_bindings
 end
 
-## Functions needed for !! and !$
-function __history_previous_command
-    switch (commandline -t)
-        case "!"
-            commandline -t $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
-end
-
-function __history_previous_command_arguments
-    switch (commandline -t)
-        case "!"
-            commandline -t ""
-            commandline -f history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
-
-# The bindings for !! and !$
-if [ "$fish_key_bindings" = fish_vi_key_bindings ]
-
-    bind -Minsert ! __history_previous_command
-    bind -Minsert '$' __history_previous_command_arguments
-else
-    bind ! __history_previous_command
-    bind '$' __history_previous_command_arguments
-end
-
 if status is-interactive
     atuin init fish | source
 end
